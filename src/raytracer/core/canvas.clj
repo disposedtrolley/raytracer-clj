4e0fb9d31031c41ce0cb0a50949c397ce417acaa
James Liu <james@jamesliu.io>
2021-03-04 21:53:07 +1100

    feat: add method to write pixels to the canvas

--- src/raytracer/core/canvas.clj
+++ src/raytracer/core/canvas.clj
@@ -1,4 +1,5 @@
-(ns raytracer.core.canvas)
+(ns raytracer.core.canvas
+  (:require [raytracer.core.tuples :as tuples]))
 
 (defn width
   [canvas]
@@ -8,6 +9,10 @@
   [canvas]
   (:height canvas))
 
+(defn pixels
+  [canvas]
+  (:pixels canvas))
+
 (defn make-canvas
   "Makes a canvas object with the provided width and
   height, prefilled with black pixels."
@@ -16,6 +21,23 @@
   ;; make a custom iterator to return each row of pixels according
   ;; to the width of the image.
   {:pixels (vec (repeat (* width height)
-                        (raytracer.core.tuples/make-colour 0 0 0)))
+                        (tuples/make-colour 0 0 0)))
    :width width
    :height height})
+
+(defn write
+  "Writes a pixel to the canvas at the x and y coordinates
+  provided, returning a new copy of the canvas."
+  [canvas x y pixel]
+  (if (or (>= x (width canvas)) (< x 0))
+    (throw (ex-info "x should be between 0 and the canvas' width-1" {:canvas canvas :x x :y y :pixel pixel})))
+  (if (or (>= y (height canvas)) (< y 0))
+    (throw (ex-info "y should be between 0 and the canvas' width-1" {:canvas canvas :x x :y y :pixel pixel})))
+  (let [pixel-idx (+ (* y (width canvas)) x)]
+    (assoc canvas :pixels (assoc (pixels canvas) pixel-idx pixel))))
+
+(defn pixel
+  "Returns the pixel at the x and y coordinates of the canvas."
+  [canvas x y]
+  (let [pixel-idx (+ (* y (width canvas)) x)]
+    (get (pixels canvas) pixel-idx)))
--- test/raytracer/core/canvas_test.clj
+++ test/raytracer/core/canvas_test.clj
@@ -14,3 +14,17 @@
              (get (:pixels test-canvas) 0)))
       (is (= true
              (tuples/is-colour? (get (:pixels test-canvas) 0)))))))
+
+(deftest write-test
+  (let [test-canvas (SUT/make-canvas 10 20)
+        red (tuples/make-colour 1 0 0)]
+    (testing "writing pixels to the canvas"
+      (let [mutated-canvas (SUT/write test-canvas 2 3 red)]
+        (is (= red
+               (SUT/pixel mutated-canvas 2 3)))))
+    (testing "writing pixels at the boundaries of the canvas"
+      (let [mutated-canvas (SUT/write (SUT/write test-canvas 0 0 red) 9 19 red)]
+        (is (= red
+               (SUT/pixel mutated-canvas 0 0)))
+        (is (= red
+               (SUT/pixel mutated-canvas 9 19)))))))
