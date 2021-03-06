4a2d401cf2fa9f2abe539ffc4d970a7ef54e1126
James Liu <james@jamesliu.io>
2021-03-06 16:08:23 +1100

    feat: new `tick` entry point plotting to a canvas

--- src/raytracer/examples/tick.clj
+++ src/raytracer/examples/tick.clj
@@ -1,5 +1,7 @@
 (ns raytracer.examples.tick
-  (:require [raytracer.core.tuples :as tuples]))
+  (:require [raytracer.core.tuples :as tuples])
+  (:require [raytracer.core.canvas :as canvas])
+  (:require [clojure.math.numeric-tower :as math]))
 
 (defn make-projectile
   "Makes a projectile from a given position point and velocity vector."
@@ -37,3 +39,30 @@
       (when (> (tuples/y (:position p)) 0)
        (printf "Tick: %d Position: %s\n" i p)
        (recur (tick p e) (inc i)))))))
+
+(defn run-with-canvas
+  "Executes the simulation with the projectile's position
+  plotted onto a canvas, returning the resulting PPM file
+  contents as a string."
+  []
+  (let [c (canvas/make-canvas 900 550)
+        p (make-projectile
+            (tuples/make-point 0 1 0)
+            (tuples/mul (tuples/normalise (tuples/make-vector 1 1.8 0)) 11.25))
+        e (make-environment
+            (tuples/make-vector -0 -0.1 0)
+            (tuples/make-vector -0.01 0 0))
+        red (tuples/make-colour 1 0 0)]
+    (loop [p p
+           c c
+           i 0]
+      (if (> (tuples/y (:position p)) 0)
+        (let [new-c (canvas/write
+                      c
+                      (int (tuples/x (:position p)))
+                      (math/abs (int (- (canvas/height c) (tuples/y (:position p))))) ;; Flip the y-coordinate.
+                      red)
+              new-p (tick p e)]
+          (printf "Tick: %d Position: %s\n" i p)
+          (recur new-p new-c (inc i)))
+        (canvas/to-ppm c)))))
